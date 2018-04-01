2f090b58183ea996a20beeb298e4e56db1f86f49
AM99999 <wonsuk.lee2@ucalgary.ca>
2018-03-31 21:03:20 -0600

    Add files via upload

---
+++ CourseSS.js
@@ -0,0 +1,11 @@
+function getCourse(){
+	let course = firebase.database().ref().child("Courses");
+	course.on("child_added", snap => {
+		
+		var course = snap.child("Name").val();
+		var semester = snap.child("Semester").val();
+		var year = snap.child("Year").val();
+		
+		$("#Courses").append("<a href='#' class='list-group-item'>" + course + ": " + semester + " " + year + "</a>");
+	});
+}
