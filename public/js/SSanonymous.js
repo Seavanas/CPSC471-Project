bf98e6765e00631b7a2cd73989db027baba6b267
AM99999 <wonsuk.lee2@ucalgary.ca>
2018-04-05 20:02:57 -0600

    Add files via upload

--- public/js/SSanonymous.js
+++ public/js/SSanonymous.js
@@ -1,30 +1,25 @@
-function createAnonPost() {
-  let post_title = $("#post_title").val();
-  let post_content = $("#post_content").val();
-  let post_ref = firebase.database().ref("Anonymous");
+function createPost($routeParams) {
+  $("#anon-post_submit").on('click', function(){
+    let title = $("#post_title").val();
+    let content = $("#post_content").val();
 
-  post_ref.once("value").then(function(snapshot){
-    let post_count = snapshot.val().Post_count;
-
-    post_count++;
-    post_ref.update({
-      Post_count: post_count
-    });
+    if(title.length > 4 && content.length > 0){
+      let postRef = firebase.database().ref("/Anonymous/"+$routeParams.Course_ID);
+      let key = postRef.push().key;
+      console.log(key);
+      postRef.child(key).set({
+        Title: title,
+        Post_content: content,
+        User_ID: firebase.auth().currentUser.uid,
+        Timestamp: firebase.database.ServerValue.TIMESTAMP
+      });
+      window.location.href = "/#!/course/" + $routeParams.Course_ID + "/Anonymous/" + key;
+    }
   });
-
-  post_ref.push().set({
-    Title: post_title,
-    Post_content: post_content,
-    Timestamp: firebase.database.ServerValue.TIMESTAMP,
-    User_ID: firebase.auth().currentUser.uid
-  });
-
-  $("#p1").text(post_content);
-  console.log(post_content);
 }
 
 function getAnonCoursePostList(routeParams) {
-  let post_ref = firebase.database().ref("Anonymous/" + routeParams.Course_ID);
+  let post_ref = firebase.database().ref("Post/" + routeParams.Course_ID);
   let post_list = [];
 
   post_ref.once("value", function(snapshot){
@@ -32,7 +27,7 @@ function getAnonCoursePostList(routeParams) {
       let Post_ID = childSnapshot.key;
       let post_title = childSnapshot.val().Title;
       firebase.database().ref("Users/" + childSnapshot.val().User_ID).once('value').then(snap => {
-        $("#anon-post_list").append("<a href='/#!/course/" + routeParams.Course_ID +"/post/"+ Post_ID + "' class='list-group-item'>"
+        $("#post_list").append("<a href='/#!/course/" + routeParams.Course_ID +"/post/"+ Post_ID + "' class='list-group-item'>"
           + "<h4>" + post_title + "</h4>"
           + "<i class='far fa-user'></i> <span> Anonymous </span>&nbsp;"
           + "<i class='far fa-clock'></i> <span>" + new moment(childSnapshot.val().Timestamp).fromNow() +"</span>"
