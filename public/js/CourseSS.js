9743fef0eb0b8093a3405510d568d5ecfeb83f75
Ryan Pang <RyanPang12@gmail.com>
2018-04-10 12:53:31 -0600

    Wonsuked

--- public/js/CourseSS.js
+++ public/js/CourseSS.js
@@ -1,10 +1,9 @@
 function getCourse(){
 	let course = firebase.database().ref().child("Courses");
 	course.on("child_added", snap => {
-
-		var course = snap.child("Name").val();
-		var semester = snap.child("Semester").val();
-		var year = snap.child("Year").val();
+		let course = snap.val().Name;
+		var semester = snap.val().Semester;
+		var year = snap.child.Year;
 
 		$("#Courses").append("<a href='#!/course/"+snap.key+"' class='list-group-item'>" + course + ": " + semester + " " + year + "</a>");
 	});
