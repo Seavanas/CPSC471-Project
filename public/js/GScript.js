52599100b4df99d84c2151ade725b123e5097b7a
GurneckJohal <gurneck.johal@ucalgary.ca>
2018-04-10 12:53:02 -0600

    updated create course
    updated created course

--- public/js/GScript.js
+++ public/js/GScript.js
@@ -1,19 +1,28 @@
 function createCourse() {
   $("#submit_course").on('click', function(){
-    let name = $("#course_name").val();
+    let code = $("#course_code").val();
+    let number = $("#course_number").val();
+    let description = $("#course_description").val();
     let semester = $("#course_semester").val();
     let year = $("#course_year").val();
-    if(name.length > 0 && semester.length > 0 && year.length > 0){
-      let storageLocation = firebase.database().ref("Courses");
-      let newCourse = storageLocation.push().key;
-      console.log(newCourse);
-      storageLocation.child(newCourse).set({
-        Admin: firebase.auth().currentUser.uid,
-        Name: name,
-        Semester: semester,
-        Year: year
+    if(code.length > 0 && number.length > 0 && description.length > 0 && semester.length > 0 && year.length > 0){
+      let storageLocation = firebase.database().ref("Courses/" + code+"_"+number);
+      storageLocation.once("value").then(function(snapshot){
+      console.log(snapshot.val());
+        if(snapshot.val()){
+          errorMessage = "Course has already been registered";
+          $("#userError").css("visibility", "visible");
+          $("#userError").text(errorMessage);
+        }
+        else{
+          storageLocation.child(code+"_"+number).set({
+            Admin: firebase.auth().currentUser.uid,
+            Name: (code+" "+number+": "+ description),
+            Semester: semester,
+            Year: year
+          });
+        }
       });
-      window.location.href = "/#!/home";
     }
   });
 }
