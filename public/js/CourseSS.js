bcaa4133b0ca13dde3e76ed8b103bf29db3a38fc
Ryan Pang <RyanPang12@gmail.com>
2018-04-10 13:07:55 -0600

    Fixed the year bug and comment bug.

--- public/js/CourseSS.js
+++ public/js/CourseSS.js
@@ -5,7 +5,7 @@ function getCourse(){
 		console.log(snap.val());
 		let course = snap.val().Name;
 		var semester = snap.val().Semester;
-		var year = snap.Year;
+		var year = snap.val().Year;
 
 		$("#Courses").append("<a href='#!/course/"+snap.key+"' class='list-group-item'>" + course + ": " + semester + " " + year + "</a>");
 	});
